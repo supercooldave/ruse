f44aef9ee9e881364931e8241607523a5eb21d26
Dave Clarke <dave.clarke@cs.kuleuven.be>
2013-06-23 20:42:52 +0200

    Basic memory safety and Stack update predicates.

--- formalism/Assembler.v
+++ formalism/Assembler.v
@@ -1,6 +1,8 @@
 Require Import Omega.
 
-Inductive Register := R0 | R1 | R2 | R3 | R4 | R5 | R6 | R7 | R8 | R9 | R10 | R11.
+Inductive Register := R0 | R1 | R2 | R3 | R4 | R5 | R6 | R7 | R8 | R9 | R10 | R11 | SP.
+Inductive Flag := SF | ZF.
+Inductive Bit := O | I.
 
 Definition Address := nat.
 Definition Value := nat.
@@ -60,8 +62,70 @@ Definition entrypoint (s : MemoryDescriptor) (p : Address) : Prop :=
 Definition data_segment (s : MemoryDescriptor) (p : Address) : Prop :=
   starting_address s + code_size s <= p /\ p < last_address s.
 
+Definition return_entrypoint (s : MemoryDescriptor) (p : Address) : Prop :=
+  p = starting_address s + no_entrypoints s * entrypoint_size.
 
-  
+Inductive read_allowed : MemoryDescriptor -> Address -> Address -> Prop :=
+  read_protected : forall (s : MemoryDescriptor) (p a : Address),
+    protected s p -> protected s a -> read_allowed s p a
+| read_unprotected : forall (s : MemoryDescriptor) (p a : Address),
+    unprotected s p -> unprotected s a -> read_allowed s p a.
+
+Inductive write_allowed : MemoryDescriptor -> Address -> Address -> Prop :=
+  write_protected : forall (s : MemoryDescriptor) (p a : Address),
+    protected s p -> data_segment s a -> write_allowed s p a
+| write_unprotected : forall (s : MemoryDescriptor) (p a : Address),
+    unprotected s a -> write_allowed s p a.
+
+Definition entry_jump (s : MemoryDescriptor) (p p' : Address) := 
+  unprotected s p /\ entrypoint s p'.
+
+Definition exit_jump (s : MemoryDescriptor) (p p' : Address) := 
+  protected s p /\ unprotected s p'.
+
+Definition int_jump (s : MemoryDescriptor) (p p' : Address) := 
+  protected s p /\ protected s p' /\ ~ data_segment s p'.
+
+
+Definition ext_jump (s : MemoryDescriptor) (p p' : Address) := 
+  unprotected s p /\ unprotected s p'.
+
+Definition same_jump  (s : MemoryDescriptor) (p p' : Address) := 
+  int_jump s p p' \/ ext_jump s p p'.
+
+Definition valid_jump  (s : MemoryDescriptor) (p p' : Address) := 
+  same_jump s p p' \/ entry_jump s p p'  \/ exit_jump s p p'.
+
+
+Definition SPsec (s : MemoryDescriptor) := starting_address s + code_size s.
+
+Definition SPext (s : MemoryDescriptor) := last_address s.
+
+
+Parameter Memory : Set.
+Parameter lookup : Memory -> Address -> Value.
+Parameter store : Memory -> Address -> Value -> Memory.
+
+Definition RegisterFile := Register -> nat.
+(*
+Definition updateR (r : RegisterFile) (reg : Register) (v : Value) : RegisterFile :=
+  fun (reg' : Register) => if (reg = reg') then v else r reg.
+*)
+Parameter updateR : RegisterFile -> Register -> Value -> RegisterFile.
+
+Definition Flags := Flag -> Bit. 
+
+Open Scope type_scope.
+Definition State := Memory * RegisterFile * Flags.
+Close Scope type_scope.
+
+Inductive set_stack : MemoryDescriptor -> Address -> RegisterFile -> Memory -> Address -> RegisterFile -> Memory -> Prop :=
+  stack_out_to_in : forall (s : MemoryDescriptor) (p p' : Address) (m m': Memory) (r r': RegisterFile),
+  entry_jump s p p' -> m' = store m (SPext s) (r SP) -> r' = updateR r SP (lookup m (SPsec s)) -> set_stack s p r m p' r' m'
+| stack_in_to_out : forall (s : MemoryDescriptor) (p p' : Address) (m m' : Memory) (r r': RegisterFile),
+  exit_jump s p p' -> m' = store m (SPsec s) (r SP) -> r' = updateR r SP (lookup m (SPext s)) -> set_stack s p r m p' r' m'
+| stack_no_change : forall (s : MemoryDescriptor) (p p' : Address) (m : Memory) (r : RegisterFile),
+  same_jump s p p' -> set_stack s p r m p' r m.
 
   
 
