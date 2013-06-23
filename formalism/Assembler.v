791e2a94c36590c398b7190d6e0dba1bfc1e3aaa
Dave Clarke <dave.clarke@cs.kuleuven.be>
2013-06-23 21:03:32 +0200

    Refactored out memory descriptor..

--- formalism/Assembler.v
+++ formalism/Assembler.v
@@ -22,31 +22,33 @@ Inductive Instruction :=
  | halt :                         Instruction.
 
 Record MemoryDescriptor := MemDesc {
-  starting_address : nat;
-  code_size : nat;
-  data_size : nat;
-  no_entrypoints: nat
+  starting_address_ : nat;
+  code_size_ : nat;
+  data_size_ : nat;
+  no_entrypoints_ : nat
 }.
 
-Definition last_address (s : MemoryDescriptor) : Address :=
-   starting_address s + code_size s + data_size s.
+Parameter s : MemoryDescriptor.
+
+Definition starting_address := starting_address_ s.
+Definition code_size := code_size_ s.
+Definition data_size := data_size_ s.
+Definition no_entrypoints := no_entrypoints_ s.
+
+Definition last_address : Address := starting_address + code_size + data_size.
 
 
-Inductive protected : MemoryDescriptor -> Address -> Prop :=
-  Prot : forall (s : MemoryDescriptor) (p : Address), 
-    starting_address s <= p -> p < last_address s -> protected s p.
+Inductive protected :Address -> Prop :=
+  Prot : forall (p : Address), starting_address <= p -> p < last_address -> protected p.
 
-Inductive unprotected : MemoryDescriptor -> Address -> Prop :=
-  Unprot_before : forall (s : MemoryDescriptor) (p : Address), 
-    p < starting_address s -> unprotected s p
-| Unprot_after :  forall (s : MemoryDescriptor) (p : Address), 
-  last_address s <= p -> unprotected s p.
+Inductive unprotected :Address -> Prop :=
+  Unprot_before : forall (p : Address), p < starting_address -> unprotected p
+| Unprot_after :  forall (p : Address), last_address <= p -> unprotected p.
 
 Lemma protected_unprotected_disjoint :
-  forall (s : MemoryDescriptor) (p : Address), not (protected s p /\ unprotected s p).
+  forall (p : Address), not (protected p /\ unprotected p).
 Proof.
-  intros s p.
-  intro.
+  intros p H.
   decompose [and] H.
   inversion H0.
   inversion H1 ; apply (lt_irrefl p); omega.
@@ -55,51 +57,39 @@ Qed.
 Parameter entrypoint_size : nat.
 Axiom non_zero_entrypoint_size : entrypoint_size > 0.
 
+Definition entrypoint (p : Address) : Prop :=
+  exists (m : nat), m < no_entrypoints /\ p = starting_address + m * entrypoint_size.  
 
-Definition entrypoint (s : MemoryDescriptor) (p : Address) : Prop :=
-  exists (m : nat), m < no_entrypoints s /\ p = starting_address s + m * entrypoint_size.  
+Definition data_segment (p : Address) : Prop :=
+  starting_address + code_size <= p /\ p < last_address.
 
-Definition data_segment (s : MemoryDescriptor) (p : Address) : Prop :=
-  starting_address s + code_size s <= p /\ p < last_address s.
+Definition return_entrypoint (p : Address) : Prop :=
+  p = starting_address + no_entrypoints * entrypoint_size.
 
-Definition return_entrypoint (s : MemoryDescriptor) (p : Address) : Prop :=
-  p = starting_address s + no_entrypoints s * entrypoint_size.
+Inductive read_allowed : Address -> Address -> Prop :=
+  read_protected : forall (p a : Address), protected p -> protected a -> read_allowed p a
+| read_unprotected : forall (p a : Address), unprotected p -> unprotected a -> read_allowed p a.
 
-Inductive read_allowed : MemoryDescriptor -> Address -> Address -> Prop :=
-  read_protected : forall (s : MemoryDescriptor) (p a : Address),
-    protected s p -> protected s a -> read_allowed s p a
-| read_unprotected : forall (s : MemoryDescriptor) (p a : Address),
-    unprotected s p -> unprotected s a -> read_allowed s p a.
+Inductive write_allowed : Address -> Address -> Prop :=
+  write_protected : forall (p a : Address), protected p -> data_segment a -> write_allowed p a
+| write_unprotected : forall (p a : Address), unprotected a -> write_allowed p a.
 
-Inductive write_allowed : MemoryDescriptor -> Address -> Address -> Prop :=
-  write_protected : forall (s : MemoryDescriptor) (p a : Address),
-    protected s p -> data_segment s a -> write_allowed s p a
-| write_unprotected : forall (s : MemoryDescriptor) (p a : Address),
-    unprotected s a -> write_allowed s p a.
+Definition entry_jump (p p' : Address) := unprotected p /\ entrypoint p'.
 
-Definition entry_jump (s : MemoryDescriptor) (p p' : Address) := 
-  unprotected s p /\ entrypoint s p'.
+Definition exit_jump (p p' : Address) := protected p /\ unprotected p'.
 
-Definition exit_jump (s : MemoryDescriptor) (p p' : Address) := 
-  protected s p /\ unprotected s p'.
+Definition int_jump (p p' : Address) := protected p /\ protected p' /\ ~ data_segment p'.
 
-Definition int_jump (s : MemoryDescriptor) (p p' : Address) := 
-  protected s p /\ protected s p' /\ ~ data_segment s p'.
+Definition ext_jump (p p' : Address) := unprotected p /\ unprotected p'.
+
+Definition same_jump (p p' : Address) := int_jump p p' \/ ext_jump p p'.
+
+Definition valid_jump (p p' : Address) := same_jump p p' \/ entry_jump p p'  \/ exit_jump p p'.
 
 
-Definition ext_jump (s : MemoryDescriptor) (p p' : Address) := 
-  unprotected s p /\ unprotected s p'.
+Definition SPsec := starting_address  + code_size.
 
-Definition same_jump  (s : MemoryDescriptor) (p p' : Address) := 
-  int_jump s p p' \/ ext_jump s p p'.
-
-Definition valid_jump  (s : MemoryDescriptor) (p p' : Address) := 
-  same_jump s p p' \/ entry_jump s p p'  \/ exit_jump s p p'.
-
-
-Definition SPsec (s : MemoryDescriptor) := starting_address s + code_size s.
-
-Definition SPext (s : MemoryDescriptor) := last_address s.
+Definition SPext := last_address.
 
 
 Parameter Memory : Set.
@@ -119,13 +109,13 @@ Open Scope type_scope.
 Definition State := Memory * RegisterFile * Flags.
 Close Scope type_scope.
 
-Inductive set_stack : MemoryDescriptor -> Address -> RegisterFile -> Memory -> Address -> RegisterFile -> Memory -> Prop :=
-  stack_out_to_in : forall (s : MemoryDescriptor) (p p' : Address) (m m': Memory) (r r': RegisterFile),
-  entry_jump s p p' -> m' = store m (SPext s) (r SP) -> r' = updateR r SP (lookup m (SPsec s)) -> set_stack s p r m p' r' m'
-| stack_in_to_out : forall (s : MemoryDescriptor) (p p' : Address) (m m' : Memory) (r r': RegisterFile),
-  exit_jump s p p' -> m' = store m (SPsec s) (r SP) -> r' = updateR r SP (lookup m (SPext s)) -> set_stack s p r m p' r' m'
-| stack_no_change : forall (s : MemoryDescriptor) (p p' : Address) (m : Memory) (r : RegisterFile),
-  same_jump s p p' -> set_stack s p r m p' r m.
+Inductive set_stack : Address -> RegisterFile -> Memory -> Address -> RegisterFile -> Memory -> Prop :=
+  stack_out_to_in : forall (p p' : Address) (m m': Memory) (r r': RegisterFile),
+  entry_jump p p' -> m' = store m SPext (r SP) -> r' = updateR r SP (lookup m SPsec) -> set_stack p r m p' r' m'
+| stack_in_to_out : forall (p p' : Address) (m m' : Memory) (r r': RegisterFile),
+  exit_jump p p' -> m' = store m SPsec (r SP) -> r' = updateR r SP (lookup m SPext) -> set_stack p r m p' r' m'
+| stack_no_change : forall (p p' : Address) (m : Memory) (r : RegisterFile),
+  same_jump p p' -> set_stack p r m p' r m.
 
   
 
