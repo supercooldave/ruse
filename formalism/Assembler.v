8a1f2a8ce01d379824f6e29bc97965aa7ad37e7a
Squera <squera.patrignani@gmail.com>
2013-06-26 11:50:37 +0200

    Added a labelled operational semantics and some sketched theorem regarding it

--- formalism/Assembler.v
+++ formalism/Assembler.v
@@ -2,9 +2,9 @@ Require Import Omega.
 Require Import List.
 
 
-(*=======================
+(*==============================================
    Syntax
-=======================*)
+==============================================*)
 
 (* TODO: Find better home for these -- better still, see if they are in Coq lib. *)
 Fixpoint ble_nat (n m : nat) : bool :=
@@ -140,13 +140,8 @@ Axiom non_zero_entrypoint_size : entrypoint_size > 0.
 
 
 
-Parameter msec : Memory.
-Parameter mext : Memory.
-Axiom dom_msec  : forall (a : Address), domain msec a <-> protected a.
-Axiom dom_mext  : forall (a : Address), domain mext a <-> ( a = 0  \/ unprotected a).
 
-Definition MemSec := { m : Memory | forall (a : Address), domain m a <-> protected a }.
-Definition MemExt := { m : Memory | forall (a : Address), domain mext a <-> ( a = 0  \/ unprotected a) }.
+
 
 (* Auxiliary functions modelling the access control policy *)
 Definition entrypoint (p : Address) : Prop :=
@@ -191,9 +186,9 @@ Inductive set_stack : Address -> RegisterFile -> Memory -> Address -> RegisterFi
 
 
 
-(*=======================
+(*==============================================
    Operational Semantics
-=======================*)
+==============================================*)
 Open Scope type_scope.
 
 (* State of the operational semantics *)
@@ -312,7 +307,17 @@ where "S '--->' S'" := (evalR S S') : type_scope.
 
 (* contextual equivalence *)
 (* TODO: eliminate Program cause redundancy? *) 
+Definition MemSec := { m : Memory | forall (a : Address), domain m a <-> protected a }.
+Definition MemExt := { m : Memory | forall (a : Address), domain m a <-> ( a = 0  \/ unprotected a) }.
+
+Parameter domainMS : MemSec -> Address -> Prop.
+Parameter domainME : MemExt -> Address -> Prop.
+
 Definition Program := Memory.
+Definition Context := MemExt.
+
+Definition compatible := fun (p : Program) (c : Context),
+  forall (a : Address), ( (domain p a) <-> (~ domainME c a) /\ (domainME c a) <-> (~ domain p a)).
 
 Inductive do_n_steps: State -> nat -> State -> Prop :=
   | do_0 : forall sta, do_n_steps sta 0 sta
@@ -333,15 +338,21 @@ Definition f_0 : Flags :=   fun f : Flag => false.
 
 Definition initial : Program -> State  :=   fun p : Program => ( p_0, r_0, f_0, p ).
 
-(* TODO: define in terms of context. Need to change the memory? *)
+Definition plug : Program -> Context -> Program := fun (p : Program) (c : Context) => 
+  p.  (* TODO: wrong, should plug p and c, not return p*)
 
-Definition contextual_equivalence : Program -> Program -> Prop :=
-  fun p1 p2 : Program => ((diverge (initial p1)) <-> (diverge (initial p2))).
+Definition contextual_equivalence : Program -> Program -> Prop := fun p1 p2 : Program => 
+  forall c : Context, compatible p1 c -> compatible p2 c ->
+    ( (diverge (initial (plug p1 c))) <-> (diverge (initial (plug p2 c))) ).
   
 
 
 
 
+(*==============================================
+   Properties
+==============================================*)
+(* TODO: a set of small lemmas that prove semi-trivial properties on MemSec, MemExt and so on *)
 
 
 
@@ -349,9 +360,11 @@ Definition contextual_equivalence : Program -> Program -> Prop :=
 
 
 
-(*=======================
+
+
+(*==============================================
    Trace Semantics
-=======================*)
+==============================================*)
 
 (* TODO:  need to define a ProtectedMemory for the trace state *) 
 (* State for the trace semantics *)
@@ -447,9 +460,9 @@ Definition trace_equivalence : Program -> Program -> Prop :=
 
 
 
-(*=======================
+(*==============================================
    Theorems 
-=======================*)
+==============================================*)
 
 Lemma trace_semantics_soundness :
   forall p1 p2: Program, trace_equivalence p1 p2 -> contextual_equivalence p1 p2.
@@ -473,6 +486,182 @@ Qed.
 
 
 
+
+
+
+
+(*==============================================
+   Labelled Operational Semantics
+==============================================*)
+
+Reserved Notation "S '~~>' S'" (at level 50, left associativity).
+
+Inductive eval_same_dom : State -> State -> Prop :=
+  sd_eval_movl : forall (p : Address) (r r' : RegisterFile) (f : Flags) (m : Memory) (rd rs : Register),
+    inst (lookup m p) (movl rd rs) -> 
+    same_jump p (S p) ->
+    read_allowed p (lookup m (r rs)) -> 
+    same_jump p (r rd) ->  (* this read rule is only for readin in the same domain of the pc *)
+    r' = updateR r rd (lookup m (r rs)) ->  
+    (p, r, f, m) ~~> (S p, r', f, m)
+
+| sd_eval_movs : forall (p : Address) (r : RegisterFile) (f : Flags) (m m' : Memory) (rd rs : Register),
+    inst (lookup m p) (movs rd rs) -> 
+    same_jump p (S p) ->
+    write_allowed p (lookup m (r rd)) -> 
+    same_jump p (r rd)->  (* this write rule is only for writing in the same domain of the pc *)
+    m' = update m (lookup m (r rs)) (r rd) ->  
+    (p, r, f, m) ~~> (S p, r, f, m')
+
+| sd_eval_movi : forall (p : Address) (i : Value) (r r' : RegisterFile) (f : Flags) (m : Memory) (rd : Register),
+    inst (lookup m p) (movi rd i) -> 
+    same_jump p (S p) ->
+    r' = updateR r rd i ->  
+    (p, r, f, m) ~~> (S p, r', f, m)
+
+| sd_eval_compare : forall (p : Address) (r : RegisterFile) (f f' : Flags) (m : Memory) (r1 r2 : Register),
+    inst (lookup m p) (cmp r1 r2) -> 
+    same_jump p (S p) ->
+    f' = updateF (updateF f ZF (beq_nat (r r1) (r r2))) SF (blt_nat (r r1) (r r2)) ->
+    (p, r, f, m) ~~> (S p, r, f', m)
+
+| sd_eval_add : forall (p : Address) (r r' : RegisterFile) (f f' : Flags) (v : Value) (m : Memory) (rd rs : Register),
+    inst (lookup m p) (add_ rd rs) -> 
+    same_jump p (S p) ->
+    v = plus (r rd) (r rs)  ->
+    r' = updateR r rd v ->
+    f' = updateF f ZF (beq_nat v 0) ->
+    (p, r, f, m) ~~> (S p, r', f', m)
+
+| sd_eval_sub : forall (p : Address) (r r' : RegisterFile) (f f' : Flags) (v : Value) (m : Memory) (rd rs : Register),
+    inst (lookup m p) (sub_ rd rs) -> 
+    same_jump p (S p) ->
+    v = minus (r rd) (r rs)  ->
+    r' = updateR r rd v ->
+    f' = updateF f ZF (beq_nat v 0) ->
+    (p, r, f, m) ~~> (S p, r', f', m)
+
+| sd_eval_call : forall (p p' : Address) (r r' r'' : RegisterFile) (f : Flags) (m m' m'' : Memory) (rd : Register),
+    inst (lookup m p) (call rd) -> 
+    p' = r rd ->
+    same_jump p p' ->  (*only jumps between the same domains*)
+    set_stack p r m p' r' m' ->
+    r'' = updateR r' SP (S (r' SP)) ->
+    m'' = update m (r'' SP) (S p) ->
+    (p, r, f, m) ~~> (p', r, f, m)
+
+| sd_eval_ret : forall (p p' : Address) (r r' r'' : RegisterFile) (f : Flags) (m m' : Memory),
+    inst (lookup m p)  ret ->
+    p' = lookup m (r SP) ->
+    same_jump p p' -> (* only jumps between the same domains*)
+    set_stack p r m p' r' m' ->
+    r'' = updateR r' SP (minus (r' SP) 1) ->
+    (p, r, f, m) ~~> (p', r'', f, m')
+
+| sd_eval_je_true : forall (p p' : Address) (r : RegisterFile) (f : Flags) (m : Memory) (ri : Register),
+    inst (lookup m p) (je ri) -> 
+    f ZF = true ->
+    p' = r ri ->
+    same_jump p p' ->
+    (p, r, f, m) ~~> (p', r, f, m)
+
+| sd_eval_je_false : forall (p p' : Address) (r : RegisterFile) (f : Flags) (m : Memory) (ri : Register),
+    inst (lookup m p) (je ri) -> 
+    f ZF = false ->
+    p' = S p ->
+    same_jump p p' ->
+    (p, r, f, m) ~~> (p', r, f, m)
+
+| sd_eval_jl_true : forall (p p' : Address) (r : RegisterFile) (f : Flags) (m : Memory) (ri : Register),
+    inst (lookup m p) (jl ri) -> 
+    f SF = true ->
+    p' = r ri ->
+    same_jump p p' ->
+    (p, r, f, m) ~~> (p', r, f, m)
+
+| sd_eval_jl_false : forall (p p' : Address) (r : RegisterFile) (f : Flags) (m : Memory) (ri : Register),
+    inst (lookup m p) (jl ri) -> 
+    f SF = false ->
+    p' = S p ->
+    same_jump p p' ->
+    (p, r, f, m) ~~> (p', r, f, m)
+
+| sd_eval_jump : forall (p p' : Address) (r : RegisterFile) (f : Flags) (m : Memory) (rd : Register),
+    inst (lookup m p) (jmp rd) -> 
+    p' = r rd ->
+    same_jump p p' ->
+    (p, r, f, m) ~~> (p', r, f, m)
+
+| sd_eval_halt : forall (p : Address) (r : RegisterFile) (f : Flags) (m : Memory),
+    inst (lookup m p) halt ->
+    (p, r, f, m) ~~> (0, r, f, m)
+
+where "S '~~>' S'" := (eval_same_dom S S') : type_scope.
+
+
+Reserved Notation "S '~~' L '~>' S'" (at level 50, left associativity).
+
+Inductive eval_trace : State -> Label -> State -> Prop :=
+  los_eval_same : forall (s s' : State),
+    s ~~> s'->
+    s ~~ Tau ~> s'
+
+| los_eval_call : forall (p p' : Address) (r r' r'' : RegisterFile) (f : Flags) (m m' m'' : Memory) (rd : Register),
+  inst (lookup m p) (call rd) -> 
+  p' = r rd ->
+  entry_jump p p' ->  
+  set_stack p r m p' r' m' ->
+  r'' = updateR r' SP (S (r' SP)) ->
+  m'' = update m (r'' SP) (S p) ->
+  (p, r, f, m) ~~ Call r f p' ~> (p', r, f, m)
+
+| los_eval_callback : forall (p p' : Address) (r r' r'' : RegisterFile) (f : Flags) (m m' m'' : Memory) (rd : Register),
+  inst (lookup m p) (call rd) -> 
+  p' = r rd ->
+  exit_jump p p' ->  
+  set_stack p r m p' r' m' ->
+  r'' = updateR r' SP (S (r' SP)) ->
+  m'' = update m (r'' SP) (S p) ->
+  (p, r, f, m) ~~ Callback r f p' ~> (p', r, f, m)
+
+| los_eval_ret : forall (p p' : Address) (r r' r'' : RegisterFile) (f : Flags) (m m' : Memory),
+    inst (lookup m p)  ret ->
+    p' = lookup m (r SP) ->
+    exit_jump p p' -> 
+    set_stack p r m p' r' m' ->
+    r'' = updateR r' SP (minus (r' SP) 1) ->
+    (p, r, f, m) ~~ Return r f ~> (p', r'', f, m')
+
+| los_eval_retback : forall (p p' : Address) (r r' r'' : RegisterFile) (f : Flags) (m m' : Memory),
+    inst (lookup m p)  ret ->
+    p' = lookup m (r SP) ->
+    entry_jump p p' -> 
+    set_stack p r m p' r' m' ->
+    r'' = updateR r' SP (minus (r' SP) 1) ->
+    (p, r, f, m) ~~ Returnback r f ~> (p', r'', f, m')
+
+where "S '~~' L '~>' S'" := (eval_trace S L S') : type_scope.
+
+
+Lemma original_semantics_implies_labelled :
+  forall s1 s2 : State,
+    s1 ---> s2 ->
+    exists l : Label, s1 ~~ l ~> s2.
+Proof.
+intros.
+destruct H.
+Admitted.
+
+
+Lemma labelles_semantics_implies_original :
+  forall (s1 s2 : State) (l : Label),
+    s1 ~~ l ~> s2 ->
+    s1 ---> s2.
+Proof.
+intros.
+destruct H. destruct H.  
+
+Admitted.
 
 (*TODO: generate labelled operational semantics
 
