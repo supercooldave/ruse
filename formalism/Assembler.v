68ee31ad39626f23957972d329cda676cb4a6646
Squera <squera.patrignani@gmail.com>
2013-06-26 14:10:24 +0200

    minor spacing

--- formalism/Assembler.v
+++ formalism/Assembler.v
@@ -30,7 +30,7 @@ Definition Bit := bool.
 
 (* Instructions of the low-level language *)
 Inductive Instruction :=
-   movl : Register -> Register -> Instruction
+ | movl : Register -> Register -> Instruction
  | movs : Register -> Register -> Instruction
  | movi : Register -> Value    -> Instruction
  | add_ : Register -> Register -> Instruction
@@ -50,6 +50,7 @@ Parameter Memory : Set.
 Parameter lookup : Memory -> Address -> Value.
 Parameter update : Memory -> Address -> Value -> Memory.
 Parameter domain : Memory -> Address -> Prop.
+
 
 (* Axioms from "Tensors of Comodels and Models for Operational Semantics" 
    by Gordon Plotkin and John Power, MFPS 2008 *)
@@ -206,104 +207,104 @@ Axiom inst_no_zero : ~ exists i : Instruction, inst 0 i.
 Reserved Notation "S '--->' S'" (at level 50, left associativity).
 
 Inductive evalR : State -> State -> Prop :=
-  eval_movl : forall (p : Address) (r r' : RegisterFile) (f : Flags) (m : Memory) (rd rs : Register),
-    inst (lookup m p) (movl rd rs) -> 
-    same_jump p (S p) ->
-    read_allowed p (lookup m (r rs)) -> 
-    r' = updateR r rd (lookup m (r rs)) ->  
-    (p, r, f, m) ---> (S p, r', f, m)
-
+| eval_movl : forall (p : Address) (r r' : RegisterFile) (f : Flags) (m : Memory) (rd rs : Register),
+  inst (lookup m p) (movl rd rs) -> 
+  same_jump p (S p) ->
+  read_allowed p (lookup m (r rs)) -> 
+  r' = updateR r rd (lookup m (r rs)) ->  
+  (p, r, f, m) ---> (S p, r', f, m)
+  
 | eval_movs : forall (p : Address) (r : RegisterFile) (f : Flags) (m m' : Memory) (rd rs : Register),
-    inst (lookup m p) (movs rd rs) -> 
-    same_jump p (S p) ->
-    write_allowed p (lookup m (r rd)) -> 
-    m' = update m (lookup m (r rs)) (r rd) ->  
-    (p, r, f, m) ---> (S p, r, f, m')
-
+  inst (lookup m p) (movs rd rs) -> 
+  same_jump p (S p) ->
+  write_allowed p (lookup m (r rd)) -> 
+  m' = update m (lookup m (r rs)) (r rd) ->  
+  (p, r, f, m) ---> (S p, r, f, m')
+  
 | eval_movi : forall (p : Address) (i : Value) (r r' : RegisterFile) (f : Flags) (m : Memory) (rd : Register),
-    inst (lookup m p) (movi rd i) -> 
-    same_jump p (S p) ->
-    r' = updateR r rd i ->  
-    (p, r, f, m) ---> (S p, r', f, m)
-
+  inst (lookup m p) (movi rd i) -> 
+  same_jump p (S p) ->
+  r' = updateR r rd i ->  
+  (p, r, f, m) ---> (S p, r', f, m)
+  
 | eval_compare : forall (p : Address) (r : RegisterFile) (f f' : Flags) (m : Memory) (r1 r2 : Register),
-    inst (lookup m p) (cmp r1 r2) -> 
-    same_jump p (S p) ->
-    f' = updateF (updateF f ZF (beq_nat (r r1) (r r2))) SF (blt_nat (r r1) (r r2)) ->
-    (p, r, f, m) ---> (S p, r, f', m)
-
+  inst (lookup m p) (cmp r1 r2) -> 
+  same_jump p (S p) ->
+  f' = updateF (updateF f ZF (beq_nat (r r1) (r r2))) SF (blt_nat (r r1) (r r2)) ->
+  (p, r, f, m) ---> (S p, r, f', m)
+  
 | eval_add : forall (p : Address) (r r' : RegisterFile) (f f' : Flags) (v : Value) (m : Memory) (rd rs : Register),
-    inst (lookup m p) (add_ rd rs) -> 
-    same_jump p (S p) ->
-    v = plus (r rd) (r rs)  ->
-    r' = updateR r rd v ->
-    f' = updateF f ZF (beq_nat v 0) ->
-    (p, r, f, m) ---> (S p, r', f', m)
-
+  inst (lookup m p) (add_ rd rs) -> 
+  same_jump p (S p) ->
+  v = plus (r rd) (r rs)  ->
+  r' = updateR r rd v ->
+  f' = updateF f ZF (beq_nat v 0) ->
+  (p, r, f, m) ---> (S p, r', f', m)
+  
 | eval_sub : forall (p : Address) (r r' : RegisterFile) (f f' : Flags) (v : Value) (m : Memory) (rd rs : Register),
-    inst (lookup m p) (sub_ rd rs) -> 
-    same_jump p (S p) ->
-    v = minus (r rd) (r rs)  ->
-    r' = updateR r rd v ->
-    f' = updateF f ZF (beq_nat v 0) ->
-    (p, r, f, m) ---> (S p, r', f', m)
-
+  inst (lookup m p) (sub_ rd rs) -> 
+  same_jump p (S p) ->
+  v = minus (r rd) (r rs)  ->
+  r' = updateR r rd v ->
+  f' = updateF f ZF (beq_nat v 0) ->
+  (p, r, f, m) ---> (S p, r', f', m)
+  
 | eval_call : forall (p p' : Address) (r r' r'' : RegisterFile) (f : Flags) (m m' m'' : Memory) (rd : Register),
-    inst (lookup m p) (call rd) -> 
-    p' = r rd ->
-    valid_jump p p' ->
-    set_stack p r m p' r' m' ->
-    r'' = updateR r' SP (S (r' SP)) ->
-    m'' = update m (r'' SP) (S p) ->
-    (p, r, f, m) ---> (p', r, f, m)
-
+  inst (lookup m p) (call rd) -> 
+  p' = r rd ->
+  valid_jump p p' ->
+  set_stack p r m p' r' m' ->
+  r'' = updateR r' SP (S (r' SP)) ->
+  m'' = update m (r'' SP) (S p) ->
+  (p, r, f, m) ---> (p', r, f, m)
+  
 | eval_ret : forall (p p' : Address) (r r' r'' : RegisterFile) (f : Flags) (m m' : Memory),
-    inst (lookup m p)  ret ->
-    p' = lookup m (r SP) ->
-    valid_jump p p' ->
-    set_stack p r m p' r' m' ->
-    r'' = updateR r' SP (minus (r' SP) 1) ->
-    (p, r, f, m) ---> (p', r'', f, m')
-
+  inst (lookup m p)  ret ->
+  p' = lookup m (r SP) ->
+  valid_jump p p' ->
+  set_stack p r m p' r' m' ->
+  r'' = updateR r' SP (minus (r' SP) 1) ->
+  (p, r, f, m) ---> (p', r'', f, m')
+  
 | eval_je_true : forall (p p' : Address) (r : RegisterFile) (f : Flags) (m : Memory) (ri : Register),
-    inst (lookup m p) (je ri) -> 
-    f ZF = true ->
-    p' = r ri ->
-    same_jump p p' ->
-    (p, r, f, m) ---> (p', r, f, m)
-
+  inst (lookup m p) (je ri) -> 
+  f ZF = true ->
+  p' = r ri ->
+  same_jump p p' ->
+  (p, r, f, m) ---> (p', r, f, m)
+  
 | eval_je_false : forall (p p' : Address) (r : RegisterFile) (f : Flags) (m : Memory) (ri : Register),
-    inst (lookup m p) (je ri) -> 
-    f ZF = false ->
-    p' = S p ->
-    same_jump p p' ->
-    (p, r, f, m) ---> (p', r, f, m)
-
+  inst (lookup m p) (je ri) -> 
+  f ZF = false ->
+  p' = S p ->
+  same_jump p p' ->
+  (p, r, f, m) ---> (p', r, f, m)
+  
 | eval_jl_true : forall (p p' : Address) (r : RegisterFile) (f : Flags) (m : Memory) (ri : Register),
-    inst (lookup m p) (jl ri) -> 
-    f SF = true ->
-    p' = r ri ->
-    same_jump p p' ->
-    (p, r, f, m) ---> (p', r, f, m)
-
+  inst (lookup m p) (jl ri) -> 
+  f SF = true ->
+  p' = r ri ->
+  same_jump p p' ->
+  (p, r, f, m) ---> (p', r, f, m)
+  
 | eval_jl_false : forall (p p' : Address) (r : RegisterFile) (f : Flags) (m : Memory) (ri : Register),
-    inst (lookup m p) (jl ri) -> 
-    f SF = false ->
-    p' = S p ->
-    same_jump p p' ->
-    (p, r, f, m) ---> (p', r, f, m)
-
+  inst (lookup m p) (jl ri) -> 
+  f SF = false ->
+  p' = S p ->
+  same_jump p p' ->
+  (p, r, f, m) ---> (p', r, f, m)
+  
 | eval_jump : forall (p p' : Address) (r : RegisterFile) (f : Flags) (m : Memory) (rd : Register),
-    inst (lookup m p) (jmp rd) -> 
-    p' = r rd ->
-    same_jump p p' ->
-    (p, r, f, m) ---> (p', r, f, m)
-
+  inst (lookup m p) (jmp rd) -> 
+  p' = r rd ->
+  same_jump p p' ->
+  (p, r, f, m) ---> (p', r, f, m)
+  
 | eval_halt : forall (p : Address) (r : RegisterFile) (f : Flags) (m : Memory),
-    inst (lookup m p) halt ->
-    (p, r, f, m) ---> (0, r, f, m)
-
-where "S '--->' S'" := (evalR S S') : type_scope.
+  inst (lookup m p) halt ->
+  (p, r, f, m) ---> (0, r, f, m)
+  
+  where "S '--->' S'" := (evalR S S') : type_scope.
 
 
 (* contextual equivalence *)
@@ -321,11 +322,11 @@ Definition compatible := fun (p : Program) (c : Context) =>
   forall (a : Address), ( (domain p a) <-> (~ domainME c a) /\ (domainME c a) <-> (~ domain p a)).
 
 Inductive do_n_steps: State -> nat -> State -> Prop :=
-  | do_0 : forall sta, do_n_steps sta 0 sta
-  | do_Sn : forall n sta sta' sta'', 
-    sta ---> sta' ->
-    do_n_steps sta' n sta'' ->
-    do_n_steps sta (S n) sta''.
+| do_0 : forall sta, do_n_steps sta 0 sta
+| do_Sn : forall n sta sta' sta'', 
+  sta ---> sta' ->
+  do_n_steps sta' n sta'' ->
+  do_n_steps sta (S n) sta''.
 
 Definition anysteps (n : nat) (sta  : State) := 
   exists n' : nat , exists sta' : State,
@@ -370,7 +371,7 @@ Definition contextual_equivalence : Program -> Program -> Prop := fun p1 p2 : Pr
 (* TODO:  need to define a ProtectedMemory for the trace state *) 
 (* State for the trace semantics *)
 Inductive TraceState := 
-  Sta : State -> TraceState
+| Sta : State -> TraceState
 | Unk : Memory -> TraceState. 
 
 (* A state is stuck if its pc is in 0 or if it cannot fetch at instruction *)
@@ -380,7 +381,7 @@ Definition stuck_state ( p: Address ) ( m : Memory ) :=
 
 (* Labels for the trace semantics*)
 Inductive Label := 
-  Tau : Label
+| Tau : Label
 | Tick : Label
 | Write_out : Address -> Value -> Label
 | Call : RegisterFile -> Flags -> Value -> Label
@@ -393,10 +394,10 @@ Inductive Label :=
 Reserved Notation " T '--' L '-->' T' " (at level 50, left associativity).
 
 Inductive trace : TraceState -> Label -> TraceState -> Prop :=
-  tr_intern : forall (p p' : Address) (r r' : RegisterFile) (f f': Flags) (m m': Memory),
-    (p, r, f, m) ---> (p', r', f', m') ->
-    int_jump p p' ->
-    Sta (p, r, f, m) -- Tau --> Sta (p', r', f', m')
+| tr_intern : forall (p p' : Address) (r r' : RegisterFile) (f f': Flags) (m m': Memory),
+  (p, r, f, m) ---> (p', r', f', m') ->
+  int_jump p p' ->
+  Sta (p, r, f, m) -- Tau --> Sta (p', r', f', m')
 
 | tr_internal_tick : forall (p p' : Address) (r r' : RegisterFile) (f f': Flags) (m m': Memory),
   (p, r, f, m) ---> (p', r', f', m') ->
@@ -435,8 +436,8 @@ where "T '--' L '-->' T'" := (trace T L T') : type_scope.
 Reserved Notation " T '==' L '==>>' T' " (at level 51, left associativity).
 
 Inductive trace_semantics : TraceState -> ( list Label ) -> TraceState -> Prop :=
-  trace_refl : forall (t : TraceState),
-    t == nil ==>> t
+| trace_refl : forall (t : TraceState),
+  t == nil ==>> t
 
 | trace_tau : forall (t t' : TraceState),
   t -- Tau --> t' ->
@@ -498,114 +499,114 @@ Qed.
 Reserved Notation "S '~~>' S'" (at level 50, left associativity).
 
 Inductive eval_same_dom : State -> State -> Prop :=
-  sd_eval_movl : forall (p : Address) (r r' : RegisterFile) (f : Flags) (m : Memory) (rd rs : Register),
-    inst (lookup m p) (movl rd rs) -> 
-    same_jump p (S p) ->
-    read_allowed p (lookup m (r rs)) -> 
-    same_jump p (r rd) ->  (* this read rule is only for readin in the same domain of the pc *)
-    r' = updateR r rd (lookup m (r rs)) ->  
+| sd_eval_movl : forall (p : Address) (r r' : RegisterFile) (f : Flags) (m : Memory) (rd rs : Register),
+  inst (lookup m p) (movl rd rs) -> 
+  same_jump p (S p) ->
+  read_allowed p (lookup m (r rs)) -> 
+  same_jump p (r rd) ->  (* this read rule is only for readin in the same domain of the pc *)
+  r' = updateR r rd (lookup m (r rs)) ->  
     (p, r, f, m) ~~> (S p, r', f, m)
-
+    
 | sd_eval_movs : forall (p : Address) (r : RegisterFile) (f : Flags) (m m' : Memory) (rd rs : Register),
-    inst (lookup m p) (movs rd rs) -> 
-    same_jump p (S p) ->
-    write_allowed p (lookup m (r rd)) -> 
-    same_jump p (r rd)->  (* this write rule is only for writing in the same domain of the pc *)
-    m' = update m (lookup m (r rs)) (r rd) ->  
-    (p, r, f, m) ~~> (S p, r, f, m')
-
+  inst (lookup m p) (movs rd rs) -> 
+  same_jump p (S p) ->
+  write_allowed p (lookup m (r rd)) -> 
+  same_jump p (r rd)->  (* this write rule is only for writing in the same domain of the pc *)
+  m' = update m (lookup m (r rs)) (r rd) ->  
+  (p, r, f, m) ~~> (S p, r, f, m')
+    
 | sd_eval_movi : forall (p : Address) (i : Value) (r r' : RegisterFile) (f : Flags) (m : Memory) (rd : Register),
-    inst (lookup m p) (movi rd i) -> 
-    same_jump p (S p) ->
-    r' = updateR r rd i ->  
-    (p, r, f, m) ~~> (S p, r', f, m)
-
+  inst (lookup m p) (movi rd i) -> 
+  same_jump p (S p) ->
+  r' = updateR r rd i ->  
+  (p, r, f, m) ~~> (S p, r', f, m)
+  
 | sd_eval_compare : forall (p : Address) (r : RegisterFile) (f f' : Flags) (m : Memory) (r1 r2 : Register),
-    inst (lookup m p) (cmp r1 r2) -> 
-    same_jump p (S p) ->
-    f' = updateF (updateF f ZF (beq_nat (r r1) (r r2))) SF (blt_nat (r r1) (r r2)) ->
-    (p, r, f, m) ~~> (S p, r, f', m)
-
+  inst (lookup m p) (cmp r1 r2) -> 
+  same_jump p (S p) ->
+  f' = updateF (updateF f ZF (beq_nat (r r1) (r r2))) SF (blt_nat (r r1) (r r2)) ->
+  (p, r, f, m) ~~> (S p, r, f', m)
+  
 | sd_eval_add : forall (p : Address) (r r' : RegisterFile) (f f' : Flags) (v : Value) (m : Memory) (rd rs : Register),
-    inst (lookup m p) (add_ rd rs) -> 
-    same_jump p (S p) ->
-    v = plus (r rd) (r rs)  ->
-    r' = updateR r rd v ->
-    f' = updateF f ZF (beq_nat v 0) ->
-    (p, r, f, m) ~~> (S p, r', f', m)
-
+  inst (lookup m p) (add_ rd rs) -> 
+  same_jump p (S p) ->
+  v = plus (r rd) (r rs)  ->
+  r' = updateR r rd v ->
+  f' = updateF f ZF (beq_nat v 0) ->
+  (p, r, f, m) ~~> (S p, r', f', m)
+  
 | sd_eval_sub : forall (p : Address) (r r' : RegisterFile) (f f' : Flags) (v : Value) (m : Memory) (rd rs : Register),
-    inst (lookup m p) (sub_ rd rs) -> 
-    same_jump p (S p) ->
-    v = minus (r rd) (r rs)  ->
-    r' = updateR r rd v ->
-    f' = updateF f ZF (beq_nat v 0) ->
-    (p, r, f, m) ~~> (S p, r', f', m)
-
+  inst (lookup m p) (sub_ rd rs) -> 
+  same_jump p (S p) ->
+  v = minus (r rd) (r rs)  ->
+  r' = updateR r rd v ->
+  f' = updateF f ZF (beq_nat v 0) ->
+  (p, r, f, m) ~~> (S p, r', f', m)
+  
 | sd_eval_call : forall (p p' : Address) (r r' r'' : RegisterFile) (f : Flags) (m m' m'' : Memory) (rd : Register),
-    inst (lookup m p) (call rd) -> 
-    p' = r rd ->
-    same_jump p p' ->  (*only jumps between the same domains*)
-    set_stack p r m p' r' m' ->
-    r'' = updateR r' SP (S (r' SP)) ->
-    m'' = update m (r'' SP) (S p) ->
-    (p, r, f, m) ~~> (p', r, f, m)
-
+  inst (lookup m p) (call rd) -> 
+  p' = r rd ->
+  same_jump p p' ->  (*only jumps between the same domains*)
+  set_stack p r m p' r' m' ->
+  r'' = updateR r' SP (S (r' SP)) ->
+  m'' = update m (r'' SP) (S p) ->
+  (p, r, f, m) ~~> (p', r, f, m)
+  
 | sd_eval_ret : forall (p p' : Address) (r r' r'' : RegisterFile) (f : Flags) (m m' : Memory),
-    inst (lookup m p)  ret ->
-    p' = lookup m (r SP) ->
-    same_jump p p' -> (* only jumps between the same domains*)
-    set_stack p r m p' r' m' ->
-    r'' = updateR r' SP (minus (r' SP) 1) ->
-    (p, r, f, m) ~~> (p', r'', f, m')
-
+  inst (lookup m p)  ret ->
+  p' = lookup m (r SP) ->
+  same_jump p p' -> (* only jumps between the same domains*)
+  set_stack p r m p' r' m' ->
+  r'' = updateR r' SP (minus (r' SP) 1) ->
+  (p, r, f, m) ~~> (p', r'', f, m')
+  
 | sd_eval_je_true : forall (p p' : Address) (r : RegisterFile) (f : Flags) (m : Memory) (ri : Register),
-    inst (lookup m p) (je ri) -> 
-    f ZF = true ->
-    p' = r ri ->
-    same_jump p p' ->
-    (p, r, f, m) ~~> (p', r, f, m)
-
+  inst (lookup m p) (je ri) -> 
+  f ZF = true ->
+  p' = r ri ->
+  same_jump p p' ->
+  (p, r, f, m) ~~> (p', r, f, m)
+  
 | sd_eval_je_false : forall (p p' : Address) (r : RegisterFile) (f : Flags) (m : Memory) (ri : Register),
-    inst (lookup m p) (je ri) -> 
-    f ZF = false ->
-    p' = S p ->
-    same_jump p p' ->
-    (p, r, f, m) ~~> (p', r, f, m)
-
+  inst (lookup m p) (je ri) -> 
+  f ZF = false ->
+  p' = S p ->
+  same_jump p p' ->
+  (p, r, f, m) ~~> (p', r, f, m)
+  
 | sd_eval_jl_true : forall (p p' : Address) (r : RegisterFile) (f : Flags) (m : Memory) (ri : Register),
-    inst (lookup m p) (jl ri) -> 
-    f SF = true ->
-    p' = r ri ->
-    same_jump p p' ->
-    (p, r, f, m) ~~> (p', r, f, m)
-
+  inst (lookup m p) (jl ri) -> 
+  f SF = true ->
+  p' = r ri ->
+  same_jump p p' ->
+  (p, r, f, m) ~~> (p', r, f, m)
+  
 | sd_eval_jl_false : forall (p p' : Address) (r : RegisterFile) (f : Flags) (m : Memory) (ri : Register),
-    inst (lookup m p) (jl ri) -> 
-    f SF = false ->
-    p' = S p ->
-    same_jump p p' ->
-    (p, r, f, m) ~~> (p', r, f, m)
-
+  inst (lookup m p) (jl ri) -> 
+  f SF = false ->
+  p' = S p ->
+  same_jump p p' ->
+  (p, r, f, m) ~~> (p', r, f, m)
+  
 | sd_eval_jump : forall (p p' : Address) (r : RegisterFile) (f : Flags) (m : Memory) (rd : Register),
-    inst (lookup m p) (jmp rd) -> 
-    p' = r rd ->
-    same_jump p p' ->
-    (p, r, f, m) ~~> (p', r, f, m)
-
+  inst (lookup m p) (jmp rd) -> 
+  p' = r rd ->
+  same_jump p p' ->
+  (p, r, f, m) ~~> (p', r, f, m)
+  
 | sd_eval_halt : forall (p : Address) (r : RegisterFile) (f : Flags) (m : Memory),
-    inst (lookup m p) halt ->
-    (p, r, f, m) ~~> (0, r, f, m)
-
-where "S '~~>' S'" := (eval_same_dom S S') : type_scope.
+  inst (lookup m p) halt ->
+  (p, r, f, m) ~~> (0, r, f, m)
+  
+  where "S '~~>' S'" := (eval_same_dom S S') : type_scope.
 
 
 Reserved Notation "S '~~' L '~>' S'" (at level 50, left associativity).
 
 Inductive eval_trace : State -> Label -> State -> Prop :=
-  los_eval_same : forall (s s' : State),
-    s ~~> s'->
-    s ~~ Tau ~> s'
+| los_eval_same : forall (s s' : State),
+  s ~~> s'->
+  s ~~ Tau ~> s'
 
 | los_eval_call : forall (p p' : Address) (r r' r'' : RegisterFile) (f : Flags) (m m' m'' : Memory) (rd : Register),
   inst (lookup m p) (call rd) -> 
@@ -626,22 +627,24 @@ Inductive eval_trace : State -> Label -> State -> Prop :=
   (p, r, f, m) ~~ Callback r f p' ~> (p', r, f, m)
 
 | los_eval_ret : forall (p p' : Address) (r r' r'' : RegisterFile) (f : Flags) (m m' : Memory),
-    inst (lookup m p)  ret ->
-    p' = lookup m (r SP) ->
-    exit_jump p p' -> 
-    set_stack p r m p' r' m' ->
-    r'' = updateR r' SP (minus (r' SP) 1) ->
-    (p, r, f, m) ~~ Return r f ~> (p', r'', f, m')
-
+  inst (lookup m p)  ret ->
+  p' = lookup m (r SP) ->
+  exit_jump p p' -> 
+  set_stack p r m p' r' m' ->
+  r'' = updateR r' SP (minus (r' SP) 1) ->
+  (p, r, f, m) ~~ Return r f ~> (p', r'', f, m')
+  
 | los_eval_retback : forall (p p' : Address) (r r' r'' : RegisterFile) (f : Flags) (m m' : Memory),
-    inst (lookup m p)  ret ->
-    p' = lookup m (r SP) ->
-    entry_jump p p' -> 
-    set_stack p r m p' r' m' ->
-    r'' = updateR r' SP (minus (r' SP) 1) ->
-    (p, r, f, m) ~~ Returnback r f ~> (p', r'', f, m')
+  inst (lookup m p)  ret ->
+  p' = lookup m (r SP) ->
+  entry_jump p p' -> 
+  set_stack p r m p' r' m' ->
+  r'' = updateR r' SP (minus (r' SP) 1) ->
+  (p, r, f, m) ~~ Returnback r f ~> (p', r'', f, m')
 
-where "S '~~' L '~>' S'" := (eval_trace S L S') : type_scope.
+(*TODO: writeout*)
+  
+  where "S '~~' L '~>' S'" := (eval_trace S L S') : type_scope.
 
 
 Lemma original_semantics_implies_labelled :
