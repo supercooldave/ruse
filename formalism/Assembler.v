79b28a7812a6df4d60737e90436e859e96349539
Squera <squera.patrignani@gmail.com>
2013-06-26 15:38:21 +0200

    "Admitted" that the original semantics implies the labelled one.

--- formalism/Assembler.v
+++ formalism/Assembler.v
@@ -695,16 +695,35 @@ Qed.
 
 
 
+(* TODO: besides the problem with the admits, i don't know how to call the other labels into place
+ there should be  5 more cases generated for them (writeout, call callback return returnback) *)
 Lemma original_semantics_implies_labelled :
   forall s1 s2 : State,
     s1 ---> s2 ->
     exists l : Label, s1 ~~ l ~> s2.
 Proof.
 intros.
-destruct H. apply ex_intro with (x := Tau).
+destruct H. 
+apply ex_intro with (x := Tau). apply los_eval_same. apply sd_eval_movl with (rs := rs) (rd := rd); auto.
+apply ex_intro with (x := Tau); apply los_eval_same. destruct H0. 
+  apply sd_eval_movs_prot with (rs := rs) (rd := rd); auto. apply or_introl; auto. destruct H0 ; auto. (* ? *) admit.
+  apply sd_eval_movs_unprot with (rs := rs) (rd := rd); auto. apply or_intror; auto. destruct H0; auto. (* ? *) admit.
+apply ex_intro with (x := Tau). apply los_eval_same. apply sd_eval_movi with (i := i) (rd := rd); auto.
+apply ex_intro with (x := Tau). apply los_eval_same. apply sd_eval_compare with (r1 := r1) (r2 := r2); auto.
+apply ex_intro with (x := Tau). apply los_eval_same. apply sd_eval_add with (rs := rs) (rd := rd) (v := v); auto.
+apply ex_intro with (x := Tau). apply los_eval_same. apply sd_eval_sub with (rs := rs) (rd := rd) (v := v); auto.
+admit.
+admit.
+apply ex_intro with (x := Tau). apply los_eval_same. apply sd_eval_je_true with (ri := ri); auto.
+apply ex_intro with (x := Tau). apply los_eval_same. apply sd_eval_je_false with (ri := ri); auto.
+apply ex_intro with (x := Tau). apply los_eval_same. apply sd_eval_jl_true with (ri := ri); auto.
+apply ex_intro with (x := Tau). apply los_eval_same. apply sd_eval_jl_false with (ri := ri); auto.
+apply ex_intro with (x := Tau). apply los_eval_same. apply sd_eval_jump with (rd := rd); auto.
+apply ex_intro with (x := Tau). apply los_eval_same. apply sd_eval_halt; auto.
+Qed.
 
 
-Admitted.
+
 (*TODO: generate labelled operational semantics
 
   from this, generate the trace semantics HOOOW *) 
